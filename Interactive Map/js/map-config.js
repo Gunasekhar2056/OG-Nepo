e582f62a77f5c1d8c838f09384671a2a4f96a5c1
AAFIA ALI <amaafiaali.ofcl@gmail.com>
2025-07-08 13:33:29 +0530

    Update map-config.js

--- Interactive Map/js/map-config.js
+++ Interactive Map/js/map-config.js
@@ -8,182 +8,182 @@ var tryjsconfig = {
   },
   "tryjs2":{
     "hover": "West Bengal<br>Total EV Sales: 78,112 ",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs3":{
     "hover": "Uttarakhand<br>Total EV Sales: 57,606",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs4":{
     "hover": "Uttar Pradesh<br>Total EV Sales: 732,074",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs5":{
     "hover": "Tripura<br>Total EV Sales: 18,346",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs6":{
     "hover": "Tamil Nadu<br>Total EV Sales: 206,902",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs7":{
     "hover": "Telangana",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs8":{
     "hover": "Sikkim<br>Total EV Sales: 4",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs9":{
     "hover": "Rajasthan<br>Total EV Sales: 228,573",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs10":{
     "hover": "Puducherry<br>Total EV Sales: 5,139",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs11":{
     "hover": "Punjab<br>Total EV Sales: 49,285",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs12":{
     "hover": "Odisha<br>Total EV Sales: 86,823",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs13":{
     "hover": "Nagaland<br>Total EV Sales: 52",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs14":{
     "hover": "Mizoram<br>Total EV Sales: 235",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs15":{
     "hover": "Madhya Pradesh<br>Total EV Sales: 133,182",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs16":{
     "hover": "Manipur<br>Total EV Sales: 1,284",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs17":{
     "hover": "Meghalaya<br>Total EV Sales: 208",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs18":{
     "hover": "Maharashtra<br>Total EV Sales: 401,535",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs19":{
     "hover": "Lakshadweep<br>Total EV Sales: 1 ",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs20":{
     "hover": "Kerala<br>Total EV Sales: 133,246",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs21":{
     "hover": "Karnataka<br>Total EV Sales: 315,498",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs22":{
     "hover": "Ladakh<br>Total EV Sales: 88",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs23":{
     "hover": "Jharkhand<br>Total EV Sales: 47,871",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs24":{
     "hover": "Haryana<br>Total EV Sales: 85,250",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs25":{
     "hover": "Himachal Pradesh<br>Total EV Sales: 2,886",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs26":{
     "hover": "Gujarat<br>Total EV Sales: 176,713",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs27":{
     "hover": "Goa<br>Total EV Sales: 17,382",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs28":{
     "hover": "Dadra and Nagar Haveli",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs29":{
     "hover": "Delhi<br>Total EV Sales: 268,538",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs30":{
     "hover": "Daman and Diu<br>Total EV Sales: 431",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs31":{
     "hover": "Chhattisgarh<br>Total EV Sales: 75,275",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs32":{
     "hover": "Chandigarh<br>Total EV Sales: 11,453",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs33":{
     "hover": "Bihar<br>Total EV Sales: 213,465",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs34":{
     "hover": "Assam<br>Total EV Sales: 151,917",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs35":{
     "hover": "Arunachal Pradesh<br>Total EV Sales: 40",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs36":{
     "hover": "Andhra Pradesh<br>Total EV Sales: 77,356",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   },
   "tryjs37":{
     "hover": "Andaman and Nicobar Islands<br>Total EV Sales: 202",
-    "upColor": "#F5F5F7", "overColor": "#C5D2E3", "downColor": "#cae9af",
+    "upColor": "#F5F5F7", "overColor": "#C5D2E3",
     "active": true
   }, 
   
